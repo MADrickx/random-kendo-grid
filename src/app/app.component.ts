1dee8a3b3cfa43fd2b6850f89680ae5e2936932b
audric <audricskivee@gmail.com>
2024-12-17 13:12:42 +0100

    update prod mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'kendo-grid-test';
   public showGrid = false;
-
   public onGridDestroy(): void {
     this.showGrid = false;
   }
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+enableProdMode();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
+  .catch((err) => console.error(err));
